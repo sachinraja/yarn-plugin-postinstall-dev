aec464990f61703441f554a206b82564fd5b2516
Sachin Raja <sachinraja2349@gmail.com>
2021-08-02 14:15:51 -0700

    chore: build

--- .yarn/plugins/@yarnpkg/plugin-postinstall-dev.cjs
+++ .yarn/plugins/@yarnpkg/plugin-postinstall-dev.cjs
@@ -2,7 +2,7 @@
 module.exports = {
 name: "@yarnpkg/plugin-postinstall-dev",
 factory: function (require) {
-var plugin;(()=>{"use strict";var e={d:(r,t)=>{for(var o in t)e.o(t,o)&&!e.o(r,o)&&Object.defineProperty(r,o,{enumerable:!0,get:t[o]})},o:(e,r)=>Object.prototype.hasOwnProperty.call(e,r),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},r={};e.r(r),e.d(r,{default:()=>o});const t=require("child_process"),o={hooks:{async afterAllInstalled(){new Promise(e=>{const r=(0,t.spawn)("yarn",["run","postinstallDev"]);r.stdout.pipe(process.stdout),r.stderr.pipe(process.stderr),r.addListener("exit",()=>e())})}}};plugin=r})();
+var plugin;(()=>{"use strict";var e={d:(t,r)=>{for(var o in r)e.o(r,o)&&!e.o(t,o)&&Object.defineProperty(t,o,{enumerable:!0,get:r[o]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};e.r(t),e.d(t,{default:()=>o});const r=require("child_process"),o={hooks:{async afterAllInstalled(){await new Promise(e=>{const t=(0,r.spawn)("yarn",["run","postinstallDev"]);t.stdout.pipe(process.stdout),t.stderr.pipe(process.stderr),t.addListener("exit",()=>e())})}}};plugin=t})();
 return plugin;
 }
 };
