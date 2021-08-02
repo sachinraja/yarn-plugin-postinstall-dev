b9447785c4b7f1b8d597d8b75a0bb5e8f7a35a9f
Sachin Raja <sachinraja2349@gmail.com>
2021-08-02 14:12:22 -0700

    feat: show command output

--- sources/index.ts
+++ sources/index.ts
@@ -1,10 +1,15 @@
 import type { Plugin } from '@yarnpkg/core'
-import { spawnSync } from 'child_process'
+import { spawn } from 'child_process'
 
 const plugin: Plugin = {
   hooks: {
-    afterAllInstalled() {
-      spawnSync('yarn', ['run', 'postinstallDev'])
+    async afterAllInstalled() {
+      await new Promise<void>((resolve) => {
+        const proc = spawn('yarn', ['run', 'postinstallDev'])
+        proc.stdout.pipe(process.stdout)
+        proc.stderr.pipe(process.stderr)
+        proc.addListener('exit', () => resolve())
+      })
     },
   },
 }
