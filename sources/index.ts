67f1febacf7ecacb5a9c4949a2a30495faf3cbc8
Sachin Raja <sachinraja2349@gmail.com>
2021-09-04 12:36:14 -0700

    fix: don't error if script does not exist in current workspace

--- sources/index.ts
+++ sources/index.ts
@@ -1,10 +1,17 @@
-import type { Plugin } from '@yarnpkg/core'
+import type { Plugin, Project } from '@yarnpkg/core'
 import { spawn } from 'child_process'
 
 const plugin: Plugin = {
   hooks: {
-    async afterAllInstalled() {
+    async afterAllInstalled(project: Project) {
       await new Promise<void>((resolve) => {
+        if (
+          !project
+            .getWorkspaceByCwd(project.cwd)
+            .manifest.scripts.has('postinstallDev')
+        )
+          return resolve()
+
         const proc = spawn('yarn', ['run', 'postinstallDev'])
         proc.stdout.pipe(process.stdout)
         proc.stderr.pipe(process.stderr)
