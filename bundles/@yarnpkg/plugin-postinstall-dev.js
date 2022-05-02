a47c78ecdd8f87540825e60e9027c09494979868
Jan Nicklas <j.nicklas@me.com>
2022-05-02 15:15:25 +0200

    use '@yarnpkg/shell'

--- bundles/@yarnpkg/plugin-postinstall-dev.js
+++ bundles/@yarnpkg/plugin-postinstall-dev.js
@@ -2,7 +2,7 @@
 module.exports = {
 name: "@yarnpkg/plugin-postinstall-dev",
 factory: function (require) {
-var plugin;(()=>{"use strict";var e={d:(r,t)=>{for(var o in t)e.o(t,o)&&!e.o(r,o)&&Object.defineProperty(r,o,{enumerable:!0,get:t[o]})},o:(e,r)=>Object.prototype.hasOwnProperty.call(e,r),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},r={};e.r(r),e.d(r,{default:()=>n});const t=require("child_process"),o="win32"===process.platform?".cmd":"",n={hooks:{async afterAllInstalled(e){await new Promise(r=>{(0,t.spawn)("yarn"+o,["run","postinstallDev"],{stdio:"inherit",cwd:e.cwd}).on("exit",()=>{r()})})}}};plugin=r})();
+var plugin;(()=>{"use strict";var e={d:(r,t)=>{for(var o in t)e.o(t,o)&&!e.o(r,o)&&Object.defineProperty(r,o,{enumerable:!0,get:t[o]})},o:(e,r)=>Object.prototype.hasOwnProperty.call(e,r),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},r={};e.r(r),e.d(r,{default:()=>o});const t=require("@yarnpkg/shell"),o={hooks:{afterAllInstalled:async e=>(0,t.execute)("yarn",["run","postinstallDev"],{cwd:e.cwd})}};plugin=r})();
 return plugin;
 }
 };
--- sources/index.ts
+++ sources/index.ts
@@ -1,21 +1,9 @@
 import type { Plugin, Project } from '@yarnpkg/core'
-import { spawn } from "child_process";
-
-const extension = process.platform === "win32" ? ".cmd" : "";
+import { execute } from '@yarnpkg/shell';
 
 const plugin: Plugin = {
   hooks: {
-    async afterAllInstalled(project: Project) {
-      await new Promise<void>((resolve) => {
-        const child = spawn(`yarn${extension}`, ['run', 'postinstallDev'], {
-          stdio: "inherit",
-          cwd: project.cwd,
-        })
-        child.on('exit', () => {
-          resolve();
-        });
-      })
-    },
+    afterAllInstalled: async (project: Project) => execute('yarn', ['run', 'postinstallDev'], { cwd: project.cwd })
   },
 }
 
