0f8b2f1e70b1dee61ef14d877bed755b17717489
Jan Nicklas <j.nicklas@me.com>
2022-05-02 10:17:21 +0200

    fix win32

--- sources/index.ts
+++ sources/index.ts
@@ -1,11 +1,13 @@
 import type { Plugin, Project } from '@yarnpkg/core'
 import { spawn } from 'cross-spawn'
 
+const extension = process.platform === "win32" ? ".cmd" : "";
+
 const plugin: Plugin = {
   hooks: {
     async afterAllInstalled(project: Project) {
       await new Promise<void>((resolve) => {
-        const proc = spawn('yarn', ['run', 'postinstallDev'], {
+        const proc = spawn(`yarn${extension}`, ['run', 'postinstallDev'], {
           cwd: project.cwd,
         })
         proc.stdout.pipe(process.stdout)
