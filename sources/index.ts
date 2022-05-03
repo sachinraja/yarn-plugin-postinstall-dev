a1f2d78348ddd506e262b05c034e0b4c6952cdbe
Sachin Raja <sachinraja2349@gmail.com>
2022-05-03 16:09:19 -0700

    use same logic as yarn's run command

--- sources/index.ts
+++ sources/index.ts
@@ -1,18 +1,20 @@
-import type { Plugin, Project } from '@yarnpkg/core'
-import { execute } from '@yarnpkg/shell'
+import { Plugin, Project, scriptUtils } from '@yarnpkg/core'
 
+const scriptName = 'postinstallDev'
+
+// @see https://github.com/yarnpkg/berry/blob/d7d260e4ee6e39ed4b3dac1fdabbed025bf99899/packages/plugin-essentials/sources/commands/run.ts#L82
 const plugin: Plugin = {
   hooks: {
     async afterAllInstalled(project: Project) {
-      const command = project.topLevelWorkspace.manifest.scripts.get('postinstallDev')
-
-      if (!command) {
-        return
+      const locator = project.topLevelWorkspace.anchoredLocator
+      if (scriptUtils.hasPackageScript(locator, scriptName, { project })) {
+        return await scriptUtils.executePackageScript(locator, scriptName, [], {
+          project,
+          stdin: process.stdin,
+          stdout: process.stdout,
+          stderr: process.stderr
+        })
       }
-
-      execute(command, [], {
-        cwd: project.cwd
-      })
     },
   },
 }
