070c4e38dea0d1eb40d00d93db638172bb8f4e6b
Sachin Raja <sachinraja2349@gmail.com>
2022-05-03 15:51:01 -0700

    allow no postinstallDev script and use @yarnpkg/shell

--- sources/index.ts
+++ sources/index.ts
@@ -1,16 +1,19 @@
 import type { Plugin, Project } from '@yarnpkg/core'
-import { spawn } from 'cross-spawn'
+import { execute } from '@yarnpkg/shell'
+
+
 
 const plugin: Plugin = {
   hooks: {
     async afterAllInstalled(project: Project) {
-      await new Promise<void>((resolve) => {
-        const proc = spawn('yarn', ['run', 'postinstallDev'], {
-          cwd: project.cwd,
-        })
-        proc.stdout.pipe(process.stdout)
-        proc.stderr.pipe(process.stderr)
-        proc.addListener('exit', () => resolve())
+      const command = project.topLevelWorkspace.manifest.scripts.get('postinstallDev')
+
+      if (!command) {
+        return
+      }
+
+      execute(command, [], {
+        cwd: project.cwd
       })
     },
   },
